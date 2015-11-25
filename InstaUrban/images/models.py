b4ed10970b3c3775647cae0f3fbdc3d28efa3ab9
Mauricio Munguia <maumg1196@gmail.com>
2015-11-24 20:39:06 -0600

    Images´ Model

--- InstaUrban/images/models.py
+++ InstaUrban/images/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from location_field.models.plain import PlainLocationField
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Place(models.Model):
+
+    # Street Artist
+    owner = models.ForeignKey(User, null=True, blank=True)
+
+    # Image
+    image = models.ImageField(upload_to="static/img/place_media", null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    # Location Info
+    city = models.CharField(max_length=255)
+    location = PlainLocationField(based_fields=[city], zoom=8)
+
+    # Meta data
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
