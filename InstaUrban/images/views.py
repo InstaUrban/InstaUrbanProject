758f64187bdbc4e0cc8b1c2ddab9d2913f9f404e
Mauricio Munguia <maumg1196@gmail.com>
2015-11-25 19:58:19 -0600

    Home showing the places to the recently to the latest

--- InstaUrban/images/views.py
+++ InstaUrban/images/views.py
@@ -6,3 +6,7 @@ from django.views.generic import TemplateView, ListView, DetailView
 class ListImage(ListView):
     model = Image
     template_name = "image_list.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.queryset = Image.objects.all().order_by('created')[::-1]
+        return super(ListImage, self).dispatch(request, *args, **kwargs)
